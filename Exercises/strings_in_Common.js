859964462b1a02aa2bffad9a5f31101c87bfb08c
Richard-Howard-Toronto <rh_gk@rogers.com>
2018-09-14 12:06:07 -0400

    very small change

--- Exercises/strings_in_Common.js
+++ Exercises/strings_in_Common.js
@@ -2,14 +2,13 @@ function stringsInCommon(cats,dogs) {
   let count = 0;
   let commonTrait = [];
   for (var i = 0; i < cats.length; ) {
-    cats[i];
     for (var j = 0; j < dogs.length; j++) {
       if (cats[i]===dogs[j]) {
       commonTrait.push(cats[i]);
         count++;
       }
       else {
-        // do nothing
+        // do nothing, keep looking
       }
     }
     cats[i++];
