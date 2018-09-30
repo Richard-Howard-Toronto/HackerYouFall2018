4be16fa442b7915490ea15172c08b1df4e4c174e
Richard-Howard-Toronto <rh_gk@rogers.com>
2018-09-30 14:14:11 -0400

    html and css for coin flip

--- Project 1 (c) Coin Flip/project_1_coin_flip.js
+++ Project 1 (c) Coin Flip/project_1_coin_flip.js
@@ -1,21 +1,10 @@
-// A coin flip is a 50/50 chance that you will get heads or tails.
-//
-// If you flip a coin 100 times, how many of them will be heads and how many will be tails? Sometimes you get 55/45, maybe even 60/40. Let's call these 100 flips a "group".
-//
-// If you repeat the previous 100 flip group 100,000 times, each 100 flips recording any record highs and record lows for that group.
-//
-// So, to approach it from a programming perspective:
-//
-// Write a flip() function that flips a coin and returns heads or tails based on a random number.
-// Write a function flipMany() that calls flip() 100 times. Recording the results of heads and tails.
-// If there are record highs or record lows for each flip group. For example, 30 heads or 70 tails, record them as the the current highs/lows.
-// Write a function flipManyGroups() that calls flipMany() 100,000. Each time the flipMany() is run, it might find a new high/low.
-// This will effectively flip a coin 10 million times. If you computer can handle it, you can increase this number. I ran mine 10,000,000 times, flipping the coin 1 billion times getting these results:
-//
-// highest heads: 75 highest tails: 76 lowest heads: 24 lowest tails: 25
+// $(document).ready(function(){
+//     // your code here
+// });
+function reFresh() {
+   location.reload();
+}
 
-
-// generate a heads or a tails
 
 let globalcount = 0;
 let arrayHeadsTailsAll = []; //
@@ -68,7 +57,10 @@ function functionName() {
     console.log(`max tails is ${Math.max(...storeElementArrayTails)}`);
     console.log(`min tails is ${Math.min(...storeElementArrayTails)}`);
 
-
+    $('span').text(`The results ${Math.min(...storeElementArrayTails)}`);
+    $('span').text(`The results ${Math.min(...storeElementArrayHeads)}`);
+    $('span').text(`The results ${Math.min(...storeElementArrayTails)}`);
+    $('span').text(`The results ${Math.min(...storeElementArrayTails)}`);
 }
 
 function flipManyGroups(num) {
@@ -82,3 +74,5 @@ function flipManyGroups(num) {
 flipManyGroups(100000)
 
 functionName();
+
+// the output
