2c455c12228c22a8cb35663ea019507cbe69682f
Richard-Howard-Toronto <rh_gk@rogers.com>
2018-10-05 20:21:52 -0400

    project 1

---
+++ Project 1 (b)/index.js
@@ -0,0 +1,6 @@
+var welcomeContent = "The rules are simple.  The rock represents evolution.  Evolution always wins, except for paper which represents the human (Anthropecene) impact on the planet and the scissors represent the ability of the planet to recover from human behavior.";
+
+function displayRules() {
+  var el = document.getElementById('displayRules');
+  el.textContent = welcomeContent;
+}
