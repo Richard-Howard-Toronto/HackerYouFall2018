1f88f233aad2da1824ab1b3be65ecbfa7fbcadac
Richard-Howard-Toronto <rh_gk@rogers.com>
2018-10-06 09:26:28 -0400

    sat morning oct 6

--- Project 1 (b)/index.js
+++ Project 1 (b)/index.js
@@ -1,6 +1,8 @@
-var welcomeContent = "The rules are simple.  The rock represents evolution.  Evolution always wins, except for paper which represents the human (Anthropecene) impact on the planet and the scissors represent the ability of the planet to recover from human behavior.";
+var welcomeContent = "The rules are simple.  The rock represents evolution.  Evolution always wins, except for paper which represents the human (Anthropecene) impact on the planet and the scissors represent the ability of the planet to recover from human behavior. Pick your choice.";
 
 function displayRules() {
   var el = document.getElementById('displayRules');
   el.textContent = welcomeContent;
+  var button = document.getElementById('button');
+  button.style.visibility = 'hidden';
 }
